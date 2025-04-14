5be6922d13c8f3c98f1c3c601353b7d26a5ff0eb
jafar <mjafer657@gmail.com>
2025-04-14 22:31:25 +0530

    Flatten Binary Tree to Linked List  - solution

--- src/april/FlattenBinaryTreeToLinkedList.java
+++ src/april/FlattenBinaryTreeToLinkedList.java
@@ -1,57 +1,37 @@
 package april;
 
 public class FlattenBinaryTreeToLinkedList {
+	
+	/**
+	 * NOTES
+	 * in the question they mentioned the linked list will be the PRE_ORDER traversal of the binary tree, hence we can just do the preorder traversal to get the answer
+	 */
 
-	TreeNode res;
+	TreeNode res = new TreeNode();
 
 	public void flatten(TreeNode root) {
-		reverseInorder(root);
+		
+		TreeNode resDup = res;
+		preOrder(root);
 
-		root = res;
+		root = resDup.right;
 	}
 
-//	private void reverseInorder(TreeNode root) {
-//		if (root == null) {
-//			return;
-//		}
-//
-//		reverseInorder(root.right);
-//
-//		TreeNode temp = root.left;
-//		if (res == null) {
-//			root.left = null;
-//			res = root;
-//		} else {
-////			res.right = root;
-//
-//			root.right = res;
-//			res = root;
-//			root.left = null;
-//		}
-//
-//		reverseInorder(temp);
-//	}
 	
-	
-	private void reverseInorder(TreeNode root) {
+	private void preOrder(TreeNode root) {
 		if (root == null) {
 			return;
 		}
-
-		reverseInorder(root.left);
-
-		TreeNode temp = root.right;
-		if (res == null) {
-			root.left = null;
-			res = root;
-		} else {
-//			res.right = root;
-
-			root.right = res;
-			res = root;
-			root.left = null;
-		}
-
-		reverseInorder(temp);
+		
+		TreeNode tempLeft = root.left;
+		TreeNode tempRight = root.right;
+		
+		res.right = root;
+		root.left = null;
+		res = root;
+		
+		preOrder(tempLeft);
+		
+		preOrder(tempRight);
 	}
 }
