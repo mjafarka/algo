4371e00eebc8f0d9deedbed21141d4106d3c16d2
mjafarka <muhammedjafarka@gmail.com>
2025-05-01 16:26:51 +0530

    regular expression matching tabular solution

--- src/april/RegularExpressionMatching.java
+++ src/april/RegularExpressionMatching.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 /**
  * check 'patter#123' in goodnotes to find why those lines are important
+ * 
+ * check out below isMatchDP solution as well.
  */
 public class RegularExpressionMatching {
 	
@@ -41,6 +43,38 @@ public class RegularExpressionMatching {
 		dp[i][j] = res ? 1 : 0;
 		
 		return res;
+	}
+	
+	/**
+	 * good notes "is match dp"
+	 * @param text
+	 * @param pattern
+	 * @return
+	 */
+	public boolean isMatchDP(String text, String pattern) {
+	    int m = text.length();
+	    int n = pattern.length();
+	    boolean[][] dp = new boolean[m + 1][n + 1];
+
+	    // Base case: empty text and empty pattern match
+	    dp[m][n] = true;
+
+	    // Fill the table from bottom to top, right to left
+	    for (int i = m; i >= 0; i--) {
+	        for (int j = n - 1; j >= 0; j--) {
+	            boolean firstMatch = (i < m) && (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.');
+
+	            if (j + 1 < n && pattern.charAt(j + 1) == '*') {
+	                dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+	            } else {
+	                dp[i][j] = firstMatch && dp[i + 1][j + 1];
+	            }
+	        }
+	    }
+
+	    System.out.println(Arrays.deepToString(dp));
+
+	    return dp[0][0];
 	}
 	
 	public static void main(String[] args) {
