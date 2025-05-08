eeaa98458ca69fb83e454a202a94973dcfc577e5
mjafarka <muhammedjafarka@gmail.com>
2025-05-08 07:45:50 +0530

    Minimum Total Distance Traveled

--- src/may/MinimumTotalDistance.java
+++ src/may/MinimumTotalDistance.java
@@ -1,35 +1,74 @@
 package may;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+
+//Minimum Total Distance Traveled
+// notes in good notes
 public class MinimumTotalDistance {
-	
-	// get time limit exceeds. need to update
+
+	// dp and non dp has the same  time complexity.
+	// space non dp => O(n+k)
+	// for dp => O(n*k)
+	private List<Integer> robot;
+	long[][] dp;
+	private List<Integer> factList;
+
 	public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-		return minTtlDistance(0, robot, factory, 0,0);
-	}
+		Collections.sort(robot);
+		Arrays.sort(factory, (a, b) -> a[0] - b[0]);
 
-	private long minTtlDistance(int r, List<Integer> robot, int[][] factory, long distance,int level) {
-		if (r == robot.size() ) {
-			return distance;
-		} else if (distance < 0 || distance > Math.pow(10,9)) {
-			return (long) Math.pow(10, 15);
-		}
-		
-		System.out.println("r ="+r+",distance ="+distance+", level ="+level);
+		List<Integer> factList = new ArrayList<Integer>();
 
-		Long minDistance = Long.MAX_VALUE;
-		for (int i = 0; i < factory.length; i++) {
-			int[] fact = factory[i];
-
-			if (fact[1] > 0) { // limit is there
-				factory[i][1]--;
-				minDistance = Math.min(minDistance,
-				minTtlDistance(r + 1, robot, factory, distance +  Math.abs(factory[i][0] - robot.get(r)),level+1));
-				factory[i][1]++;
+		for (int[] fact : factory) {
+			for (int i = 0; i < fact[1]; i++) {
+				factList.add(fact[0]);
 			}
 		}
+		this.robot = robot;
+		this.factList = factList;
+		f = new long[robot.size()][factory.length];
 
-		return minDistance;
+		dp = new long[robot.size() + 1][factList.size() + 1];
+
+		for (long[] d : dp) {
+			Arrays.fill(d, -1);
+
+		}
+		return dfs(0, 0);
+	}
+
+	private long dfs(int i, int j) {
+		if (i == robot.size()) {
+			return 0;
+		}
+		if (j >= factList.size()) {
+			return (long) (Math.pow(10, 15) + 7);
+		}
+		// if (f[i][j] != 0) {
+		// return (int) f[i][j];
+		// }
+		if (dp[i][j] != -1) {
+			return dp[i][j];
+		}
+
+		long include = Long.MAX_VALUE;
+		long notInclude = Long.MAX_VALUE;
+
+		// if (factory[j][0] > 0) {
+		// factory[j][0]--;
+		include = Math.abs(robot.get(i) - factList.get(j)) + dfs(i + 1, j + 1);
+		// factory[j][0]++;
+
+		// }
+
+		notInclude = dfs(i, j + 1);
+
+		dp[i][j] = Math.min(include, notInclude);
+
+		return dp[i][j];
 	}
 }
