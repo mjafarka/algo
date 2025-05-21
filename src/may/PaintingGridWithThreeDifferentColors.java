63b68af7aae5b27e88186e1701fed3a9e09939fe
mjafarka <muhammedjafarka@gmail.com>
2025-05-21 23:51:44 +0530

    painting grid with three different color - bit masking solution

--- src/may/PaintingGridWithThreeDifferentColors.java
+++ src/may/PaintingGridWithThreeDifferentColors.java
@@ -8,7 +8,13 @@ import java.util.List;
 /* Painting a Grid With Three Different Colors */
 
 /* notes in onenote */
+
+/* two type of solution is there
+ * 1. bit masking
+ * 2. using string as previous state
+ */
 public class PaintingGridWithThreeDifferentColors {
+
 	private List<String> columnStates = new ArrayList<>();
 	private int[][] t;
 	private final int MOD = 1_000_000_007;
@@ -76,4 +82,44 @@ public class PaintingGridWithThreeDifferentColors {
 
 		return result;
 	}
+	
+	
+	/*
+	 * bit masking solution
+	 * ---------------------
+	 */
+//	private static final int MOD = (int)1e9 + 7;
+    private int[][] state_mem = new int[1002][1024]; // 1000 rows + 2, 1024 for 10 bits
+    
+    public int colorTheGrid2(int m, int n) {
+        for (int i = 0; i < state_mem.length; i++) {
+            Arrays.fill(state_mem[i], -1);
+        }
+        return countWays(m, n, 0, 0, 0, 0);
+    }
+    
+    private int countWays(int m, int n, int r, int c, int curr_state, int prev_state) {
+        if (c == n) return 1;
+        if (r == m) return countWays(m, n, 0, c + 1, 0, curr_state);
+        if (r == 0 && state_mem[c][prev_state] != -1) return state_mem[c][prev_state];
+        
+        int up_color = 0;
+        if (r > 0) up_color = curr_state & 3;
+        
+        int left_color = (prev_state >> ((m - r - 1) * 2)) & 3;
+        
+        int ways_to_color = 0;
+        for (int color = 1; color <= 3; color++) {
+            if (color != up_color && color != left_color) {
+                ways_to_color = (ways_to_color + countWays(m, n, r + 1, c, (curr_state << 2) | color, prev_state)) % MOD;
+            }
+        }
+        
+        if (r == 0) {
+            state_mem[c][prev_state] = ways_to_color;
+        }
+        return ways_to_color;
+    }
+	
+	
 }
