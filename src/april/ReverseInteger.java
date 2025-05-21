6397555e4d5dd704ef5415fb27cf5d56859739dd
mjafarka <muhammedjafarka@gmail.com>
2025-05-21 11:33:44 +0530

    reverse Integer

--- src/april/ReverseInteger.java
+++ src/april/ReverseInteger.java
@@ -3,6 +3,8 @@ package april;
 
 // Reverse Integer
 public class ReverseInteger {
+	
+	// little easier and legible one below
 	public int reverse(int x) {
 		int result = 0;
 
@@ -18,4 +20,22 @@ public class ReverseInteger {
 
 		return result;
 	}
+	
+	public int reverseE(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (
+                
+                (rev == Integer.MAX_VALUE / 10 && pop > 7)
+            ) return 0;
+            if (
+                
+                (rev == Integer.MIN_VALUE / 10 && pop < -8)
+            ) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
 }
