4a4ffdb07b8915a44297a115bf0bed31dfbd2426
mjafarka <muhammedjafarka@gmail.com>
2025-07-09 09:30:07 +0530

    Find All K-Distant Indices in an Array

---
+++ src/july/FindAllKDistantIndicesArr.java
@@ -0,0 +1,24 @@
+package july;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Find All K-Distant Indices in an Array
+public class FindAllKDistantIndicesArr {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> res = new ArrayList<>();
+        int r = 0; // unjudged minimum index
+        int n = nums.length;
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] == key) {
+                int l = Math.max(r, j - k);
+                r = Math.min(n - 1, j + k) + 1;
+                for (int i = l; i < r; ++i) {
+                    res.add(i);
+                }
+            }
+        }
+        return res;
+    }
+}
