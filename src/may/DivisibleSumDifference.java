09e259d126727243006e87e5f8b713ccdf0994b8
mjafarka <muhammedjafarka@gmail.com>
2025-07-23 03:08:44 +0530

    divisible sum different notes update

--- src/may/DivisibleSumDifference.java
+++ src/may/DivisibleSumDifference.java
@@ -15,9 +15,27 @@ public class DivisibleSumDifference {
 
     //     return sumNotDivisible - mMultipleSum
 
+
+    /**
+     *  nums1 = totalSum - divisibleSum
+     * 
+     *  nums2 = divisibleSum
+     * 
+     *  result = totalSum - nums2 => totalSum - 2* divisibleSum
+     * 
+     *  divisibleSum calculation
+     *  m + 2m+ 3m + 4m .... xm (which is just less than n)
+     * 
+     *  m * ( x * (x+1)  / 2 )
+     * 
+     *  2 will be cancelled from the numberator and denominator
+     * @param n
+     * @param m
+     * @return
+     */
     public int differenceOfSums(int n, int m) {
         int totalSum = n * (n + 1) / 2;
-        int divisibleSum = m * (n / m) * (n / m + 1);
+        int divisibleSum = m * (n / m) * (n / m + 1); 
         return totalSum - divisibleSum;
     }
 }
