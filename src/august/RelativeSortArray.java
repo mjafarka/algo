4352986d500422eccc4c9d2bd6fae949dccc6d3b
mjafarka <muhammedjafarka@gmail.com>
2025-08-17 23:52:05 +0530

    relative sort array

---
+++ src/august/RelativeSortArray.java
@@ -0,0 +1,26 @@
+package august;
+
+
+// relative sort array
+// goodnotes
+
+// https://leetcode.com/problems/relative-sort-array/solutions/335056/java-in-place-solution-using-counting-sort
+public class RelativeSortArray {
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] cnt = new int[1001];
+        for(int n : arr1) cnt[n]++;
+        int i = 0;
+        for(int n : arr2) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        for(int n = 0; n < cnt.length; n++) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        return arr1;
+    }
+}
